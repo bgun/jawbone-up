545386983942136e92087b56e8913cc8a280fc9f
Ben Gundersen <ben@bengundersen.com>
2013-11-18 02:15:21 -0500

    cleanup

--- web.js
+++ web.js
@@ -1,8 +1,12 @@
+/**
+ *   test implementation of the jawbone-up library. suuper hacky
+ */
+
 var express = require("express");
 var app = express();
 app.use(express.static(__dirname + "/public"));
 
-var JawboneUpClient = require("./jawbone-up");
+var JawboneUpClient = require("./lib/jawbone-up");
 var client = new JawboneUpClient({
   clientId : '5Le5Fk6S4JA',
   appSecret: 'fc6b15312b6da7038c9b4b2f7215f1ecfe7a8add'
@@ -11,7 +15,7 @@ var client = new JawboneUpClient({
 app.get('/auth/url', function(req, res) {
   var oAuthUrl = client.getAuthorizeUrl({
     scope: ['basic_read','extended_read','location_read','friends_read','cardiac_read','meal_read','move_read','mood_read'].join(' '),
-    redirectURI: "https://jawbone-up-client.herokuapp.com/oauth"
+    redirect_uri: "https://jawbone-up-client.herokuapp.com/oauth"
   });
   res.set("Content-Type","application/json");
   res.set("Access-Control-Allow-Origin","*");
@@ -49,7 +53,7 @@ app.get("/oauth", function(req, res) {
 app.get("/up/*", function(req, res) {
   var token = req.query.token;
   var endpoint = req.path.substr("/up/".length,req.path.length);
-  client.setToken(token).get(endpoint,function(json) {
+  client.setToken(token).proxy(endpoint,function(json) {
     res.set("Content-Type","application/json");
     res.set("Access-Control-Allow-Origin","*");
     res.send(json);
