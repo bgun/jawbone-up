0eeff6653568753c9f6a81405894d5a427ea0374
Ben Gundersen <ben@bengundersen.com>
2013-11-18 02:34:35 -0500

    test

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ jawbone-up
 ==========
 
 Jawbone Up client for node.js
+
+See web.js for a sample implementation using Express.
--- web.js
+++ web.js
@@ -1,49 +1,41 @@
 /**
- *   test implementation of the jawbone-up library. suuper hacky
+ *  Sample implementation of the jawbone-up library using Express
  */
 
 var express = require("express");
 var app = express();
-app.use(express.static(__dirname + "/public"));
 
 var JawboneUpClient = require("./lib/jawbone-up");
 var client = new JawboneUpClient({
-  clientId : '5Le5Fk6S4JA',
-  appSecret: 'fc6b15312b6da7038c9b4b2f7215f1ecfe7a8add'
+  // fill in with info from Jawbone Developer portal
+  clientId : 'MY-CLIENT-ID',
+  appSecret: 'MY-APP-SECRET'
 });
 
-app.get('/auth/url', function(req, res) {
+app.get('/oauth/url', function(req, res) {
   var oAuthUrl = client.getAuthorizeUrl({
-    scope: ['basic_read','extended_read','location_read','friends_read','cardiac_read','meal_read','move_read','mood_read'].join(' '),
+    scope: ['basic_read',
+            'cardiac_read',
+            'extended_read',
+            'friends_read',
+            'location_read',
+            'meal_read',
+            'mood_read',
+            'move_read'
+           ].join(' '),
     redirect_uri: "https://jawbone-up-client.herokuapp.com/oauth"
   });
   res.set("Content-Type","application/json");
-  res.set("Access-Control-Allow-Origin","*");
   res.send(JSON.stringify({
     url: oAuthUrl
   }));
 });
 
-app.get("/token", function(req, res) {
-  res.set("Content-Type","application/json");
-  res.set("Access-Control-Allow-Origin","*");
-  if(app.token) {
-    res.send(JSON.stringify({
-      token: app.token
-    }));
-  } else {
-    res.send(JSON.stringify({
-      token: null,
-      error: "No token found"
-    }));
-  }
-});
-
 app.get("/oauth", function(req, res) {
   if(req.query && req.query.code) {
     client.getAccessToken(req.query.code, function(token) {
-      app.token = token;
-      res.send("Connection successful. Please close this window.");
+      // go store the token somewhere nice.
+      res.send("Houston, we have a token.");
     });
   } else {
     throw new Error("no code");
@@ -51,18 +43,16 @@ app.get("/oauth", function(req, res) {
 });
 
 app.get("/up/*", function(req, res) {
+  // look ma, no hands!
   var token = req.query.token;
   var endpoint = req.path.substr("/up/".length,req.path.length);
   client.setToken(token).proxy(endpoint,function(json) {
     res.set("Content-Type","application/json");
-    res.set("Access-Control-Allow-Origin","*");
     res.send(json);
   });
 });
 
 
-
 // start listening
-
 var port = process.env.PORT || 5000;
 app.listen(port);
